f42ff054e014533d4c695dc19b32a9880acd4d43
markosanchez800 <markosanchez800@gmail.com>
2021-05-13 18:33:58 -0700

    finished apiRoutes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -18,22 +18,20 @@ router.get('/api/workouts', (req,res) => {
         res.json(dbWorkout);
     })
     .catch(err => {
-        
+        res.status(400).json(err);
+    });
+});
+
+router.get('/api/workouts/range', (req,res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
     })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 
 module.exports = router;
